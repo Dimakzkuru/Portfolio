89d15b7488135de77215bcce5192a7839facfdb9
Dimakzkuru <michaeldimakilingece@gmail.com>
2024-06-19 20:22:57 +0800

    added some hover underline effect on navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,9 @@ import { LuSun } from "react-icons/lu";
 import { useState } from "react";
 import { HiOutlineHome } from "react-icons/hi";
 import { GoPerson } from "react-icons/go";
+import { HiOutlinePresentationChartLine } from "react-icons/hi";
+import { BiSolidContact } from "react-icons/bi";
+
 const Navbar = () => {
   const [isDark, setIsDark] = useState(false);
 
@@ -25,34 +28,36 @@ const Navbar = () => {
       <div className="flex items-center justify-center gap-10 m-8">
         <a
           href="#"
-          className=" text-lg font-semibold justify-center px-5 items-center flex"
+          className="relative link text-lg font-semibold justify-center px-5 items-center flex hover:text-red"
         >
           <HiOutlineHome className="text-lg font-semibold flex items-center justify-center mr-1 mb-0.5" />
           Home
         </a>
         <a
           href="#"
-          className=" text-lg font-semibold justify-center px-5 items-center flex"
+          className="relative link text-lg font-semibold justify-center px-5 items-center flex hover:text-red"
         >
           <GoPerson className="text-lg font-semibold flex items-center justify-center mr-1 mb-0.5" />
           About
         </a>
         <a
           href="#"
-          className="text-lg font-semibold justify-center px-5 items-center flex"
+          className="relative link text-lg font-semibold justify-center px-5 items-center flex  hover:text-red"
         >
+          <HiOutlinePresentationChartLine className="text-lg font-semibold flex items-center justify-center mr-1 mb-0.5" />
           Works
         </a>
         <a
           href="#"
-          className="text-lg font-semibold justify-center px-5 items-center flex"
+          className="relative link text-lg font-semibold justify-center px-5 items-center flex hover:text-red"
         >
+          <BiSolidContact className="text-lg font-semibold flex items-center justify-center mr-1 mb-0.5" />
           Contact
         </a>
 
         <button
           onClick={toggleTheme}
-          className="transition duration-0.5 ease-in-out"
+          className="transition duration-0.5 ease-in-out "
         >
           {isDark ? (
             <MdDarkMode className="hover:cursor-pointer text-2xl transition duration-5000 ease-in-out" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,45 +4,60 @@ export default {
   darkMode: "class",
   theme: {
     extend: {
-      content: {
-        after: '""',
-      },
       inset: {
-        "-10": "-2.5rem",
+        "-10": "-10px",
       },
-      height: {
-        "3px": "3px",
+      spacing: {
+        "70vw": "70vw",
+      },
+      minWidth: {
+        600: "600px",
       },
       transitionProperty: {
         width: "width",
       },
       backgroundColor: {
         "pink-500": "#ff3c78",
+        dark: "#0a0a0a",
+        "gray-500": "#a0a0a0",
+        white: "#ffffff",
+      },
+      colors: {
+        dark: "#0a0a0a",
+        "gray-500": "#a0a0a0",
+        white: "#ffffff",
+        red: "#FB5936",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
   variants: {
     extend: {
-      width: ["after"],
+      width: ["hover", "after"],
+      textColor: ["hover"],
     },
   },
   plugins: [
     function ({ addUtilities }) {
       addUtilities(
         {
-          ".a-after": {
-            position: "absolute",
+          ".link:after": {
             content: '""',
+            position: "absolute",
             height: "3px",
             width: "0",
             left: "0",
-            bottom: "-2.5rem",
-            backgroundColor: "#ff3c78",
-            transitionProperty: "width",
-            transitionDuration: "300ms",
+            bottom: "-10px",
+            backgroundColor: "#FB5936",
+            transition: "width 0.3s",
+          },
+          ".link:hover:after": {
+            width: "100%",
           },
         },
-        ["before", "after"]
+        ["before", "after", "hover"]
       );
     },
   ],
