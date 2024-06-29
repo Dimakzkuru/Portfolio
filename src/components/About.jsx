425110c0673bf50197476d03d3388e999d818595
Dimakzkuru <michaeldimakilingece@gmail.com>
2024-06-29 22:50:16 +0800

    added some contents for about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,35 @@
+import { BsPersonCircle } from "react-icons/bs";
+import AboutMe from "../assets/AboutMe3.png";
 const About = () => {
   return (
     <div className="border-b border-white dark:border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-2xl">
-        About <span className="text-neutral-300">Me</span>
+      <h2
+        className="my-20 flex items-center justify-center
+       text-2xl font-semibold"
+      >
+        <BsPersonCircle className="mr-2" />
+        About&nbsp;
+        <span className="text-neutral-300 dark:text-black"> Me</span>
       </h2>
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="">
+            <img src={AboutMe} alt="aboutMe" className="" />
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2">
+          <div className="flex justify-center">
+            <div className="container border-solid text-white border-2 rounded-full h-12 flex justify-around">
+              <button className="bg-red rounded-full w-full mx-1">
+                Button1
+              </button>
+              <button className="bg-slate-600 rounded-full w-full mx-1">
+                Button2
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 export const HERO_CONTENT = `I'm a self-taught front-end developer with a passion for building engaging and user-friendly web applications. My curiosity drives me to create intuitive and visually appealing interfaces that enhance user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT =
+  "I am a dedicated and versatile self-taught web developer who transitioned from Electronics Engineering to pursue a passion for creating efficient and user-friendly web applications. Although I have no professional experience yet, I have honed my skills in HTML, CSS, JavaScript, and React through personal projects and continuous learning. My journey in web development began with a deep curiosity for technology, and it has evolved into a commitment to mastering front-end development. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I stay active, explore new technologies, and contribute to open-source projects.";
 
 export const EXPERIENCES = [
   {
